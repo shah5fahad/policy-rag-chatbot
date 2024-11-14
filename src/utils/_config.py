8d7493ac3f583d508647cdd00cc4ac06601eca44
Kanha Upadhyay <kanha.upadhyay@sifars.com>
2024-11-14 14:50:23 +0530

    refactor config loading and improve environment variable checks

--- src/utils/_config.py
+++ src/utils/_config.py
@@ -3,32 +3,28 @@ import os
 from dotenv import load_dotenv
 from loguru import logger
 
-if os.getenv('PYTHON_ENV') == 'test':
-    load_dotenv(dotenv_path='.env.test', override=True)
+if os.getenv("PYTHON_ENV") == "test":
+    load_dotenv(dotenv_path=".env.test", override=True)
 else:
     load_dotenv()
 
 
 LOG_FILE = os.getenv("LOG_FILE")
 if LOG_FILE:
-    logger.add(LOG_FILE, retention="10 days")
+    LOG_RETENTION = os.getenv("LOG_RETENTION", "90 days")
+    logger.add(LOG_FILE, retention=LOG_RETENTION)
 
-if not os.getenv("SQLALCHEMY_DATABASE_URL"):
-    logger.error("SQLALCHEMY_DATABASE_URL environment not set.")
-    raise ValueError("SQLALCHEMY_DATABASE_URL environment not set.")
+if not os.getenv("SQLALCHEMY_DATABASE_URI"):
+    raise ValueError("SQLALCHEMY_DATABASE_URI environment not set.")
 
 if not os.getenv("OPENAI_API_KEY"):
-    logger.error("OPENAI_API_KEY environment not set.")
     raise ValueError("OPENAI_API_KEY environment not set.")
 
 if not os.getenv("SCHEDULER_INTERVAL"):
-    logger.error("SCHEDULER_INTERVAL environment not set.")
     raise ValueError("SCHEDULER_INTERVAL environment not set.")
 
 if not os.getenv("PINECONE_API_KEY"):
-    logger.error("PINECONE_API_KEY environment not set.")
     raise ValueError("PINECONE_API_KEY environment not set.")
 
 if not os.getenv("PINECONE_INDEX_NAME"):
-    logger.error("PINECONE_INDEX_NAME environment not set.")
     raise ValueError("PINECONE_INDEX_NAME environment not set.")
