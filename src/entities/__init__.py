5d181a29783d6934e371ce489962f7ff164793d5
Kanha Upadhyay <kanha.upadhyay@sifars.com>
2025-09-09 16:18:30 +0530

    update init file for entities

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from .audit_log import *
 from .base import *
-from .test import *
 
 api_router = APIRouter(prefix="/v1")
 
-api_router.include_router(TestController().router, prefix="/tests", tags=["tests"])
+api_router.include_router(
+    AuditLogController().router, prefix="/audit-log", tags=["audit-log"]
+)
